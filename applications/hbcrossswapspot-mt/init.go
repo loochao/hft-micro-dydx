dd674c521a5703971384a58e70d538e8482548d0
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-04-16 02:27:04 +0800

    build hft-mirco-hbcrossswapspot-mt.20210415

--- applications/hbcrossswapspot-mt/init.go
+++ applications/hbcrossswapspot-mt/init.go
@@ -106,7 +106,7 @@ var hbConfig *Config
 
 func init() {
 
-	logger.Debug("####  BUILD @ 20210415 18:17:15  ####")
+	logger.Debug("####  BUILD @ 20210415 18:27:02  ####")
 
 	configPath := flag.String("config", "", "config path")
 	flag.Parse()
