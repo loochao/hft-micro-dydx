f09abd0d1949149f0ffdb0ad9328d0510f6715ee
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-04-18 14:39:14 +0800

    build hft-mirco-kcperpspot-mt.20210418

--- applications/kcperpspot-mt/init.go
+++ applications/kcperpspot-mt/init.go
@@ -101,7 +101,7 @@ var kcConfig *Config
 
 func init() {
 
-	logger.Debug("####  BUILD @ 20210418 06:38:08  ####")
+	logger.Debug("####  BUILD @ 20210418 06:39:13  ####")
 
 	configPath := flag.String("config", "", "config path")
 	flag.Parse()
