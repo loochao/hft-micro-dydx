577e2dd13a453468d7009994ddeeff6dbef84cef
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-04-25 23:47:22 +0800

    build hft-mirco-okspotbnswap-mt.20210425

--- applications/okspotbnswap-mt/init.go
+++ applications/okspotbnswap-mt/init.go
@@ -95,7 +95,7 @@ var mtConfig *Config
 
 func init() {
 
-	logger.Debug("####  BUILD @ 20210425 15:46:47  ####")
+	logger.Debug("####  BUILD @ 20210425 15:47:21  ####")
 
 	configPath := flag.String("config", "", "config path")
 	flag.Parse()
