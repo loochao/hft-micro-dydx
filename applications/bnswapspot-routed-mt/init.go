d6b17f7cea11d2d93260115c40ca567cb7d70d3e
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-04-26 19:31:04 +0800

    build hft-mirco-bnswapspot-routed-mt.20210426

--- applications/bnswapspot-routed-mt/init.go
+++ applications/bnswapspot-routed-mt/init.go
@@ -117,7 +117,7 @@ const bnBNBSymbol = "BNBUSDT"
 
 func init() {
 
-	logger.Debug("####  BUILD @ 20210425 00:13:22  ####")
+	logger.Debug("####  BUILD @ 20210426 11:31:04  ####")
 
 	configPath := flag.String("config", "", "config path")
 	flag.Parse()
