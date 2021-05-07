283e26f8c881b46328f9cc647ccdb142e3898cb4
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-05-07 11:40:02 +0800

    build hft-recorder-okspot-trade.20210507

---
+++ tools/okspot-trade.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+dt=$(date -u +%Y%m%d)
+version=" BUILD @ $(date -u '+%Y%m%d %H:%M:%S') "
+echo "$version"
+
+env GOOS=linux GOARCH=amd64 go build -o "./dist/hft-recorder-okspot-trade.$dt" ./recorders/okspot-trade
+
+git add -A
+git commit -m "build hft-recorder-okspot-trade.$dt"
+git push origin master
+
+chmod 755 "./dist/hft-recorder-okspot-trade.$dt"
+
+echo "tokyo3"
+rsync -avx --progress "./dist/hft-recorder-okspot-trade.$dt" luchao:/usr/local/bin/
