685f80f6170abe9d4b702ef1d3c7f164926b7fc1
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-05-26 21:23:37 +0800

    build hft-mirco-opt-tt.20210526

--- applications/opt-tt/init.go
+++ applications/opt-tt/init.go
@@ -87,7 +87,7 @@ var yExchange common.Exchange
 
 func init() {
 
-	logger.Debug("####  BUILD @ 20210526 13:22:08  ####")
+	logger.Debug("####  BUILD @ 20210526 13:23:36  ####")
 
 	configPath := flag.String("config", "", "config path")
 	flag.Parse()
