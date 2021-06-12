a51161d7d642f9222b5f205086a7cdd2f0cfbd54
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-06-13 07:39:26 +0800

    build hft-mirco-opt-tt.20210612

--- applications/opt-tt/init.go
+++ applications/opt-tt/init.go
@@ -87,7 +87,7 @@ var yTimedPositionChange *common.TimedSum
 
 func init() {
 
-	logger.Debug("####  BUILD @ 20210612 18:57:52  ####")
+	logger.Debug("####  BUILD @ 20210612 23:39:24  ####")
 
 	configPath := flag.String("config", "", "config path")
 	flag.Parse()
