a9a01556161a4954553eabea6d649eaf98d3b127
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-06-14 11:21:43 +0800

    build coin-tt.20210614

--- applications/coin-tt/init.go
+++ applications/coin-tt/init.go
@@ -89,7 +89,7 @@ var yTimedPositionChange *common.TimedSum
 
 func init() {
 
-	logger.Debug("####  BUILD @ 20210614 03:20:05  ####")
+	logger.Debug("####  BUILD @ 20210614 03:21:42  ####")
 
 	configPath := flag.String("config", "", "config path")
 	flag.Parse()
