b8f7c167d8f25e8e674e7df64853fc11392f610f
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-06-18 23:26:45 +0800

    build usdt-opt-mt.20210618

--- applications/usdt-opt-mt/strategy.go
+++ applications/usdt-opt-mt/strategy.go
@@ -404,8 +404,8 @@ func (strat *XYStrategy) walkSpread() {
 		strat.xDepthExpireCount++
 	}
 	strat.depthMatchCount++
-	strat.shortLastEnter = (strat.yWalkedDepth.BidPrice - strat.xWalkedDepth.AskPrice) / strat.xWalkedDepth.AskPrice
-	strat.longLastEnter = (strat.yWalkedDepth.AskPrice - strat.xWalkedDepth.BidPrice) / strat.xWalkedDepth.BidPrice
+	strat.shortLastEnter = (strat.yWalkedDepth.BidPrice - strat.xWalkedDepth.BidPrice) / strat.xWalkedDepth.BidPrice
+	strat.longLastEnter = (strat.yWalkedDepth.AskPrice - strat.xWalkedDepth.AskPrice) / strat.xWalkedDepth.AskPrice
 
 	strat.shortEnterTimedMedian.Insert(strat.spreadTime, strat.shortLastEnter)
 	strat.longEnterTimedMedian.Insert(strat.spreadTime, strat.longLastEnter)
