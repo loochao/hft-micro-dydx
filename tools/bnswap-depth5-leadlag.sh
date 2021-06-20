86068d7c9ab96611698ba111c42d84ddb1858459
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-06-20 09:23:20 +0800

    build bnswap-depth5-leadlag.20210620

---
+++ tools/bnswap-depth5-leadlag.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+dt=$(date -u +%Y%m%d)
+version=" BUILD @ $(date -u '+%Y%m%d %H:%M:%S') "
+echo "$version"
+
+env GOOS=linux GOARCH=amd64 go build -o "./dist/bnswap-depth5-leadlag.$dt" ./recorders/bnswap-depth5-leadlag
+
+git add -A
+git commit -m "build bnswap-depth5-leadlag.$dt"
+git push origin master
+
+chmod 755 "./dist/bnswap-depth5-leadlag.$dt"
+
+echo "hk01"
+rsync -avx --progress "./dist/bnswap-depth5-leadlag.$dt" hk01:/usr/local/bin/
+
