c03e89ccbeb888d5edb4987c2a34dbef2e205c14
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-06-20 21:56:59 +0800

    build usd-ll-tt.20210620

--- tools/usd-ll-tt.sh
+++ tools/usd-ll-tt.sh
@@ -12,7 +12,8 @@ env GOOS=linux GOARCH=amd64 go build -o "./dist/usd-ll-tt.amd64.$dt" ./applicati
 git add -A
 git commit -m "build usd-ll-tt.$dt"
 git push origin master
-git tag -a "usd-ll-tt.$dt"
+git tag -d "usd-ll-tt.$dt"
+git tag "usd-ll-tt.$dt"
 git push origin "usd-ll-tt.$dt" --force
 
 chmod 755 "./dist/usd-ll-tt.amd64.$dt"
