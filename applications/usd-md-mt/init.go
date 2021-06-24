9effaa66e0176867225abcbfe73e4fd11ccd726a
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-06-24 23:40:32 +0800

    build usd-md-mt.20210624

--- applications/usd-md-mt/init.go
+++ applications/usd-md-mt/init.go
@@ -3,5 +3,5 @@ package main
 import "github.com/geometrybase/hft-micro/logger"
 
 func init() {
-	logger.Debug("####  BUILD @ 20210624 15:40:13  ####")
+	logger.Debug("####  BUILD @ 20210624 15:40:31  ####")
 }
--- applications/usd-md-mt/spread.go
+++ applications/usd-md-mt/spread.go
@@ -40,9 +40,9 @@ func (strat *XYStrategy) walkSpread() {
 	if strat.shortEnterTimedMedian.Range() < strat.config.SpreadLookback/2 {
 		return
 	}
-	if time.Now().Sub(time.Now().Truncate(time.Second*15)) < time.Millisecond*100 {
-		logger.Debugf("%s %v < %f, %v > %f", strat.xSymbol, strat.longEnterTimedMedian.Median(), strat.longBot, strat.shortEnterTimedMedian.Median(), strat.shortTop)
-	}
+	//if time.Now().Sub(time.Now().Truncate(time.Second*15)) < time.Millisecond*100 {
+	//	logger.Debugf("%s %v < %f, %v > %f", strat.xSymbol, strat.longEnterTimedMedian.Median(), strat.longBot, strat.shortEnterTimedMedian.Median(), strat.shortTop)
+	//}
 	strat.spread = &common.XYSpread{
 		ShortLastEnter:   strat.shortLastEnter,
 		ShortLastLeave:   strat.longLastEnter,
