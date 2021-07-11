fc522ab74469377f0903c2950f1950a1388d359a
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-07-11 23:39:33 +0800

    build usd-ll-mt-q.20210711

--- applications/usd-ll-mt-q/init.go
+++ applications/usd-ll-mt-q/init.go
@@ -3,5 +3,5 @@ package main
 import "github.com/geometrybase/hft-micro/logger"
 
 func init() {
-	logger.Debug("####  BUILD @ 20210708 16:00:03  ####")
+	logger.Debug("####  BUILD @ 20210711 15:39:30  ####")
 }
---
+++ tools/usd-ll-mt-q.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+dt=$(date -u +%Y%m%d)
+version=" BUILD @ $(date -u '+%Y%m%d %H:%M:%S') "
+echo "$version"
+sed -i "" -E "s/####.+####/#### $version ####/g" ./applications/usd-ll-mt-q/init.go
+
+env GOOS=linux GOARCH=arm64 go build -o "./dist/usd-ll-mt-q.arm64.$dt" ./applications/usd-ll-mt-q
+env GOOS=linux GOARCH=amd64 go build -o "./dist/usd-ll-mt-q.amd64.$dt" ./applications/usd-ll-mt-q
+
+chmod 755 "./dist/usd-ll-mt-q.amd64.$dt"
+chmod 755 "./dist/usd-ll-mt-q.arm64.$dt"
+
+git add -A
+git commit -m "build usd-ll-mt-q.$dt"
+git push origin master
+
+git tag -d "usd-ll-mt-q.$dt"
+git tag "usd-ll-mt-q.$dt"
+git push origin "usd-ll-mt-q.$dt" --force
+
+echo ""
+echo "vcarm02"
+rsync -avx --progress "./dist/usd-ll-mt-q.arm64.$dt" vcarm02:/usr/local/bin/
+
+echo ""
+echo "arm1"
+rsync -avx --progress "./dist/usd-ll-mt-q.arm64.$dt" arm1:/usr/local/bin/
