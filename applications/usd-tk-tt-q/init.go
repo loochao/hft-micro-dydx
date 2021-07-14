90603f4f7ae8de63e24036946ea93a10bf39c170
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-07-14 22:05:23 +0800

    build usd-tk-tt-q.20210714

--- applications/usd-tk-tt-q/init.go
+++ applications/usd-tk-tt-q/init.go
@@ -3,5 +3,5 @@ package main
 import "github.com/geometrybase/hft-micro/logger"
 
 func init() {
-	logger.Debug("####  BUILD @ 20210714 13:55:44  ####")
+	logger.Debug("####  BUILD @ 20210714 14:05:21  ####")
 }
--- kucoin-usdtfuture/kcuf.go
+++ kucoin-usdtfuture/kcuf.go
@@ -275,8 +275,10 @@ func (k *KucoinUsdtFuture) StreamBasic(ctx context.Context, statusCh chan common
 			}
 		case wsPosition := <-userWS.PositionCh:
 			if position, ok := positionsMap[wsPosition.Symbol]; ok {
-				logger.Debugf("POSITION %s %s %v", position.Symbol, position.EventTime)
-				if position.EventTime.Sub(wsPosition.EventTime) > 0 {
+				//logger.Debugf("POSITION %s %s %v", position.Symbol, position.EventTime)
+				//这儿需要防止和order更新的仓位挨得太近, 重复变更仓位的问题，所以ws的仓位默认需要有一个delay
+				//最近三秒的都不要
+				if wsPosition.EventTime.Sub(position.EventTime) < time.Second*3 {
 					continue
 				}
 				position.EventTime = wsPosition.EventTime
