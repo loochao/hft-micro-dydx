8a59a7930d55fce269b65a0801ede5ffc6b6f757
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-07-17 00:39:24 +0800

    build hbuf-bnuf-depth-and-ticker.20210716

--- recorders/hbuf-bnuf-depth-and-ticker/build.sh
+++ recorders/hbuf-bnuf-depth-and-ticker/build.sh
@@ -6,14 +6,14 @@ dt=$(date -u +%Y%m%d)
 version=" BUILD @ $(date -u '+%Y%m%d %H:%M:%S') "
 echo "$version"
 
-env GOOS=linux GOARCH=amd64 go build -o "./dist/hbuf-bnuf-depth5-and-ticker.$dt" ./recorders/hbuf-bnuf-depth5-and-ticker
+env GOOS=linux GOARCH=amd64 go build -o "./dist/hbuf-bnuf-depth-and-ticker.$dt" ./recorders/hbuf-bnuf-depth-and-ticker
 
 git add -A
-git commit -m "build hbuf-bnuf-depth5-and-ticker.$dt"
+git commit -m "build hbuf-bnuf-depth-and-ticker.$dt"
 git push origin master
 
-chmod 755 "./dist/hbuf-bnuf-depth5-and-ticker.$dt"
+chmod 755 "./dist/hbuf-bnuf-depth-and-ticker.$dt"
 
 echo "hk01"
-rsync -avx --progress "./dist/hbuf-bnuf-depth5-and-ticker.$dt" hk03:/usr/local/bin/
+rsync -avx --progress "./dist/hbuf-bnuf-depth-and-ticker.$dt" hk03:/usr/local/bin/
 
