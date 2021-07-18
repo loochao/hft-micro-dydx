599261e72bcd40978523a6baa53001c9e303d2a6
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-07-18 16:07:44 +0800

    build bbuf-bnuf-depth-and-ticker.20210718

--- recorders/bbuf-bnuf-depth-and-ticker/build.sh
+++ recorders/bbuf-bnuf-depth-and-ticker/build.sh
@@ -6,7 +6,7 @@ dt=$(date -u +%Y%m%d)
 version=" BUILD @ $(date -u '+%Y%m%d %H:%M:%S') "
 echo "$version"
 
-env GOOS=linux GOARCH=amd64 go build -o "./dist/bbbf-bnuf-depth-and-ticker.$dt" ./recorders/bbuf-bnuf-depth-and-ticker
+env GOOS=linux GOARCH=amd64 go build -o "./dist/bbuf-bnuf-depth-and-ticker.$dt" ./recorders/bbuf-bnuf-depth-and-ticker
 
 git add -A
 git commit -m "build bbuf-bnuf-depth-and-ticker.$dt"
