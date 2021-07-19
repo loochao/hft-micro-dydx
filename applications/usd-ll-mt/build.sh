305100f0bb8d4a03fe90ec2e2ba58ec4362b3a8c
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-07-19 22:06:11 +0800

    build usd-ll-mt.20210719

---
+++ applications/usd-ll-mt/build.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+cd ../../
+
+dt=$(date -u +%Y%m%d)
+version=" BUILD @ $(date -u '+%Y%m%d %H:%M:%S') "
+echo "$version"
+sed -i "" -E "s/####.+####/#### $version ####/g" ./applications/usd-ll-mt/init.go
+
+
+env GOOS=linux GOARCH=arm64 go build -o "./dist/usd-ll-mt.arm64.$dt" ./applications/usd-ll-mt
+env GOOS=linux GOARCH=amd64 go build -o "./dist/usd-ll-mt.amd64.$dt" ./applications/usd-ll-mt
+
+chmod 755 "./dist/usd-ll-mt.amd64.$dt"
+chmod 755 "./dist/usd-ll-mt.arm64.$dt"
+
+git add -A
+git commit -m "build usd-ll-mt.$dt"
+git push origin master
+
+git tag -d "usd-ll-mt.$dt"
+git tag "usd-ll-mt.$dt"
+git push origin "usd-ll-mt.$dt" --force
+
+
+echo ""
+echo "vcarm01"
+rsync -avx --progress "./dist/usd-ll-mt.arm64.$dt" vcarm01:/usr/local/bin/
+
+echo ""
+echo "vcarm02"
+rsync -avx --progress "./dist/usd-ll-mt.arm64.$dt" vcarm02:/usr/local/bin/
+
+echo ""
+echo "vcarm03"
+rsync -avx --progress "./dist/usd-ll-mt.arm64.$dt" vcarm03:/usr/local/bin/
+
+echo ""
+echo "arm1"
+rsync -avx --progress "./dist/usd-ll-mt.arm64.$dt" arm1:/usr/local/bin/
+
+echo "arm2"
+rsync -avx --progress "./dist/usd-ll-mt.arm64.$dt" arm2:/usr/local/bin/
+
+echo "arm3"
+rsync -avx --progress "./dist/usd-ll-mt.arm64.$dt" arm3:/usr/local/bin/
+
+echo "arm4"
+rsync -avx --progress "./dist/usd-ll-mt.arm64.$dt" arm4:/usr/local/bin/
+
--- applications/usd-ll-mt/init.go
+++ applications/usd-ll-mt/init.go
@@ -3,5 +3,5 @@ package main
 import "github.com/geometrybase/hft-micro/logger"
 
 func init() {
-	logger.Debug("####  BUILD @ 20210708 17:25:53  ####")
+	logger.Debug("####  BUILD @ 20210719 14:06:09  ####")
 }
