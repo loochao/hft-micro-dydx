9620e036e11ec09d1af2422fde278f6306bc6cc4
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-08-01 21:53:14 +0800

    build usd-md-mt.20210801

--- binance-busdfuture/bnbf-limits.go
+++ binance-busdfuture/bnbf-limits.go
@@ -3,29 +3,35 @@ package binance_busdfuture
 var TickSizes = map[string]float64{
 	"BTCBUSD": 0.1,
 	"ETHBUSD": 0.01,
+	"BNBBUSD": 0.01,
 }
 
 var StepSizes = map[string]float64{
-	"ETHBUSD": 0.001,
+	"BNBBUSD": 0.01,
 	"BTCBUSD": 0.001,
+	"ETHBUSD": 0.001,
 }
 
 var MinSizes = map[string]float64{
 	"BTCBUSD": 0.001,
 	"ETHBUSD": 0.001,
+	"BNBBUSD": 0.01,
 }
 
 var MinNotional = map[string]float64{
 	"BTCBUSD": 5,
 	"ETHBUSD": 5,
+	"BNBBUSD": 5,
 }
 
 var MultiplierUps = map[string]float64{
-	"BTCBUSD": 1.05,
-	"ETHBUSD": 1.15,
+	"BTCBUSD": 1.1,
+	"ETHBUSD": 1.1,
+	"BNBBUSD": 1.05,
 }
 
 var MultiplierDowns = map[string]float64{
-	"BTCBUSD": 0.95,
-	"ETHBUSD": 0.85,
+	"BNBBUSD": 0.95,
+	"BTCBUSD": 0.9,
+	"ETHBUSD": 0.9,
 }
