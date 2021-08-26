259b7fe32ab449283772da97195050a3e8eb1916
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-08-26 17:48:01 +0800

    build usd-tk-tt-q.20210826

--- applications/usd-tk-tt-q/init.go
+++ applications/usd-tk-tt-q/init.go
@@ -3,5 +3,5 @@ package main
 import "github.com/geometrybase/hft-micro/logger"
 
 func init() {
-	logger.Debug("####  BUILD @ 20210826 09:44:12  ####")
+	logger.Debug("####  BUILD @ 20210826 09:48:00  ####")
 }
