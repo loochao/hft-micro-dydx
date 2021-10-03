1a0105eb0b9129a0984770cf74bfc39928c3f753
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-10-04 02:11:51 +0800

    build usd-tk-xt-q.20211003

--- applications/usd-tk-xt-q/build.sh
+++ applications/usd-tk-xt-q/build.sh
@@ -7,19 +7,19 @@ version=" BUILD @ $(date -u '+%Y%m%d %H:%M:%S') "
 echo "$version"
 sed -i "" -E "s/####.+####/#### $version ####/g" ./applications/usd-tk-xt-q/init.go
 
-env GOOS=linux GOARCH=arm64 go build -o "./dist/usd-tk-xt-qt.arm64.$dt" ./applications/usd-tk-xt-q
-env GOOS=linux GOARCH=amd64 go build -o "./dist/usd-tk-xt-qt.amd64.$dt" ./applications/usd-tk-xt-q
+env GOOS=linux GOARCH=arm64 go build -o "./dist/usd-tk-xt-q.arm64.$dt" ./applications/usd-tk-xt-q
+env GOOS=linux GOARCH=amd64 go build -o "./dist/usd-tk-xt-q.amd64.$dt" ./applications/usd-tk-xt-q
 
 git add -A
-git commit -m "build usd-tk-xt-qt.$dt"
+git commit -m "build usd-tk-xt-q.$dt"
 git push origin master
-git tag -d "usd-tk-xt-qt.$dt"
-git tag "usd-tk-xt-qt.$dt"
-git push origin "usd-tk-xt-qt.$dt" --force
+git tag -d "usd-tk-xt-q.$dt"
+git tag "usd-tk-xt-q.$dt"
+git push origin "usd-tk-xt-q.$dt" --force
 
-chmod 755 "./dist/usd-tk-xt-qt.arm64.$dt"
+chmod 755 "./dist/usd-tk-xt-q.arm64.$dt"
 
-echo "" && echo "" && echo "hk01"
-rsync -avx --progress "./dist/usd-tk-xt-qt.arm64.$dt" arm4:/usr/local/bin/
+echo "" && echo "" && echo "arm4"
+rsync -avx --progress "./dist/usd-tk-xt-q.arm64.$dt" arm4:/usr/local/bin/
 
 
--- applications/usd-tk-xt-q/init.go
+++ applications/usd-tk-xt-q/init.go
@@ -3,5 +3,5 @@ package main
 import "github.com/geometrybase/hft-micro/logger"
 
 func init() {
-	logger.Debug("####  BUILD @ 20210930 02:24:30  ####")
+	logger.Debug("####  BUILD @ 20211003 18:11:46  ####")
 }
