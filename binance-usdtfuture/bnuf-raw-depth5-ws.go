ace1e39692589ffe0d58bcb554df5894beb52685
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-10-05 01:52:53 +0800

    build dxuf-bnuf-depth-and-ticker.20211004

--- binance-usdtfuture/bnuf-raw-depth5-ws.go
+++ binance-usdtfuture/bnuf-raw-depth5-ws.go
@@ -149,10 +149,12 @@ func (w *RawDepth5WS) reconnect(ctx context.Context, wsUrl string, proxy string,
 		dialer = &websocket.Dialer{
 			Proxy:            http.ProxyURL(proxyUrl),
 			HandshakeTimeout: 60 * time.Second,
+			EnableCompression: true,
 		}
 	} else {
 		dialer = &websocket.Dialer{
 			HandshakeTimeout: 10 * time.Second,
+			EnableCompression: true,
 		}
 	}
 	conn, _, err := dialer.DialContext(
