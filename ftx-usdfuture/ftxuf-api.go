44da581078268e7b8e6a2003a0ef31f7bb782c48
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-10-24 01:53:49 +0800

    build usd-tk-tt-q.20211023

--- ftx-usdfuture/ftxuf-api.go
+++ ftx-usdfuture/ftxuf-api.go
@@ -7,7 +7,6 @@ import (
 	"errors"
 	"fmt"
 	"github.com/geometrybase/hft-micro/common"
-	"github.com/geometrybase/hft-micro/logger"
 	"io"
 	"io/ioutil"
 	"net"
@@ -139,7 +138,7 @@ func (api *API) SendAuthenticatedHTTPRequest(ctx context.Context, method, path s
 	if err != nil {
 		return err
 	}
-	logger.Debugf("%s", contents)
+	//logger.Debugf("%s", contents)
 	err = resp.Body.Close()
 	if err != nil {
 		return err
