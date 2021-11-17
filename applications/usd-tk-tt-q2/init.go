757de111197203ff598bb51f01f17f199b934768
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-11-17 19:57:07 +0800

    build usd-tk-tt-q2.20211117

--- applications/usd-tk-tt-q2/init.go
+++ applications/usd-tk-tt-q2/init.go
@@ -3,5 +3,5 @@ package main
 import "github.com/geometrybase/hft-micro/logger"
 
 func init() {
-	logger.Debug("####  BUILD @ 20211117 10:58:45  ####")
+	logger.Debug("####  BUILD @ 20211117 11:57:06  ####")
 }
--- applications/usd-tk-tt-q2/order.go
+++ applications/usd-tk-tt-q2/order.go
@@ -97,10 +97,10 @@ func (strat *XYStrategy) updateXPosition() {
 		time.Now().Sub(strat.xOrderSilentTime) < 0 {
 		if time.Now().Sub(strat.logSilentTime) > 0 {
 			strat.logSilentTime = time.Now().Add(strat.config.LogInterval)
-			logger.Debugf("%s %v %v %v %v %v %v %v",
+			logger.Debugf("%s %v %v %v %v %v %v %v %v %v",
 				strat.xSymbol,
-				time.Now().Sub(strat.xPositionUpdateTime) > strat.config.AccountMaxAge,
-				time.Now().Sub(strat.yPositionUpdateTime) > strat.config.AccountMaxAge,
+				strat.xPositionUpdateTime, time.Now().Sub(strat.xPositionUpdateTime) > strat.config.AccountMaxAge,
+				strat.yPositionUpdateTime, time.Now().Sub(strat.yPositionUpdateTime) > strat.config.AccountMaxAge,
 				strat.xAccount == nil,
 				strat.yAccount == nil,
 				strat.fundingRateSettleSilent,
