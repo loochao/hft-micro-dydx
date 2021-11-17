0a869472f1ebc71b16e551b728f026401a3742b4
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-11-17 22:54:31 +0800

    build usd-tk-tt-q2.20211117

--- applications/usd-tk-tt-q2/strategy.go
+++ applications/usd-tk-tt-q2/strategy.go
@@ -405,6 +405,7 @@ func (strat *XYStrategy) handleXPosition(nextPos common.Position) {
 			logger.Debugf("%10s bad strat.xPosition == nextPos pass same pointer", strat.xSymbol)
 			return
 		}
+		logger.Debugf("X POS %s %v %v %f %f", strat.xSymbol, nextPos.GetEventTime(), strat.xPosition.GetEventTime(), math.Abs(strat.xPosition.GetSize()-nextPos.GetSize()), strat.xStepSize)
 		if nextPos.GetEventTime().Sub(strat.xPosition.GetEventTime()) >= 0 {
 			//logger.Debugf("%s %v %v %f %f", strat.xSymbol, nextPos.GetEventTime(), strat.xPosition.GetEventTime(), math.Abs(strat.xPosition.GetSize()-nextPos.GetSize()), strat.xStepSize)
 			if math.Abs(strat.xPosition.GetSize()-nextPos.GetSize()) >= strat.xStepSize {
