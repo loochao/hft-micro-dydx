0ce10818976bcb59fdc36791480325d70455f12b
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-11-26 02:23:08 +0800

    build usd-tk-tt-q2.20211125

--- applications/usd-tk-tt-q2/build.sh
+++ applications/usd-tk-tt-q2/build.sh
@@ -20,6 +20,7 @@ env GOOS=linux GOARCH=amd64 go build -o "./dist/usd-tk-tt-q2.amd64.$dt" ./applic
 chmod 755 "./dist/usd-tk-tt-q2.amd64.$dt"
 
 rsync -avx --progress "./dist/usd-tk-tt-q2.arm64.$dt" tka1:/usr/local/bin/
+rsync -avx --progress "./dist/usd-tk-tt-q2.arm64.$dt" tka2:/usr/local/bin/
 
 #echo "" && echo "" && echo "way"
 #rsync -avx --progress "./dist/usd-tk-tt-q2.amd64.$dt" way:/usr/local/bin/
--- applications/usd-tk-tt-q2/config_test.go
+++ applications/usd-tk-tt-q2/config_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	binance_busdspot "github.com/geometrybase/hft-micro/binance-busdspot"
 	binance_usdtfuture "github.com/geometrybase/hft-micro/binance-usdtfuture"
 	binance_usdtspot "github.com/geometrybase/hft-micro/binance-usdtspot"
 	dydx_usdfuture "github.com/geometrybase/hft-micro/dydx-usdfuture"
@@ -98,6 +99,30 @@ func TestShowBnusBnufPairsAndMaxSizes(t *testing.T) {
 	symbols := make([]string, 0)
 	for xSymbol := range binance_usdtspot.TickSizes {
 		ySymbol := strings.Replace(xSymbol, "USDT", "USDT", -1)
+		if yMaxPosSize, ok := binance_usdtfuture.MaxPosSizes[ySymbol]; ok {
+			symbols = append(symbols, xSymbol)
+			symbolMap[xSymbol] = ySymbol
+			maxPosSizes[xSymbol] = yMaxPosSize * 0.5
+		}
+	}
+	sort.Strings(symbols)
+	fmt.Printf("\n\nxyPairs:\n")
+	for _, xSymbol := range symbols {
+		fmt.Printf("  %s: %s\n", xSymbol, symbolMap[xSymbol])
+	}
+	fmt.Printf("\n\nmaxPosSizes:\n")
+	for _, xSymbol := range symbols {
+		fmt.Printf("  %s: %.0f\n", xSymbol, maxPosSizes[xSymbol])
+	}
+}
+
+
+func TestShowBnbsBnufPairsAndMaxSizes(t *testing.T) {
+	symbolMap := make(map[string]string)
+	maxPosSizes := make(map[string]float64)
+	symbols := make([]string, 0)
+	for xSymbol := range binance_busdspot.TickSizes {
+		ySymbol := strings.Replace(xSymbol, "BUSD", "USDT", -1)
 		if yMaxPosSize, ok := binance_usdtfuture.MaxPosSizes[ySymbol]; ok {
 			symbols = append(symbols, xSymbol)
 			symbolMap[xSymbol] = ySymbol
