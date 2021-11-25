b6671a90e1ef0ab785ae7b4a6ba119fd0a043584
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-11-25 20:49:35 +0800

    build rtt-bnus.20211125

--- round-trip-times/bnus/build.sh
+++ round-trip-times/bnus/build.sh
@@ -5,25 +5,25 @@ cd ../../
 dt=$(date -u +%Y%m%d)
 version=" BUILD @ $(date -u '+%Y%m%d %H:%M:%S') "
 echo "$version"
-sed -i "" -E "s/####.+####/#### $version ####/g" ./round-trip-times/bnuf/init.go
+sed -i "" -E "s/####.+####/#### $version ####/g" ./round-trip-times/bnus/init.go
 
 git add -A
-git commit -m "build rtt-bnuf.$dt"
+git commit -m "build rtt-bnus.$dt"
 git push origin master
-git tag -d "rtt-bnuf.$dt"
-git tag "rtt-bnuf.$dt"
-git push origin "rtt-bnuf.$dt" --force
+git tag -d "rtt-bnus.$dt"
+git tag "rtt-bnus.$dt"
+git push origin "rtt-bnus.$dt" --force
 
-env GOOS=linux GOARCH=arm64 go build -o "./dist/rtt-bnuf.arm64.$dt" ./round-trip-times/bnuf
-env GOOS=linux GOARCH=amd64 go build -o "./dist/rtt-bnuf.amd64.$dt" ./round-trip-times/bnuf
+env GOOS=linux GOARCH=arm64 go build -o "./dist/rtt-bnus.arm64.$dt" ./round-trip-times/bnus
+env GOOS=linux GOARCH=amd64 go build -o "./dist/rtt-bnus.amd64.$dt" ./round-trip-times/bnus
 
-chmod 755 "./dist/rtt-bnuf.arm64.$dt"
+chmod 755 "./dist/rtt-bnus.arm64.$dt"
 
 echo "" && echo "" && echo "way"
-rsync -avx --progress "./dist/rtt-bnuf.arm64.$dt" way:/usr/local/bin/
-rsync -avx --progress "./dist/rtt-bnuf.amd64.$dt" way:/usr/local/bin/
+rsync -avx --progress "./dist/rtt-bnus.arm64.$dt" way:/usr/local/bin/
+rsync -avx --progress "./dist/rtt-bnus.amd64.$dt" way:/usr/local/bin/
 
-ssh way "rsync -avx --progress /usr/local/bin/rtt-bnuf.arm64.$dt nv1:/usr/local/bin/"
-ssh way "rsync -avx --progress /usr/local/bin/rtt-bnuf.arm64.$dt nv2:/usr/local/bin/"
-ssh way "rsync -avx --progress /usr/local/bin/rtt-bnuf.arm64.$dt arm1:/usr/local/bin/"
+ssh way "rsync -avx --progress /usr/local/bin/rtt-bnus.arm64.$dt nv1:/usr/local/bin/"
+ssh way "rsync -avx --progress /usr/local/bin/rtt-bnus.arm64.$dt nv2:/usr/local/bin/"
+ssh way "rsync -avx --progress /usr/local/bin/rtt-bnus.arm64.$dt arm1:/usr/local/bin/"
 
--- round-trip-times/bnus/init.go
+++ round-trip-times/bnus/init.go
@@ -5,5 +5,5 @@ import (
 )
 
 func init() {
-	fmt.Printf("####  BUILD @ 20211125 12:35:54  ####\n")
+	fmt.Printf("####  BUILD @ 20211125 12:49:34  ####\n")
 }
