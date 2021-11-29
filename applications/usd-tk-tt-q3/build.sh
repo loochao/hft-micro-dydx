f8bed9dfe24c6eca4ae9f20e8b28b2078dd8b4b7
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-11-30 01:50:23 +0800

    build usd-tk-tt-q3.20211129

--- applications/usd-tk-tt-q3/build.sh
+++ applications/usd-tk-tt-q3/build.sh
@@ -22,3 +22,7 @@ chmod 755 "./dist/usd-tk-tt-q3.arm64.$dt"
 
 rsync -avx --progress "./dist/usd-tk-tt-q3.arm64.$dt" tkc1:/usr/local/bin/
 rsync -avx --progress "./dist/usd-tk-tt-q3.arm64.$dt" tka2:/usr/local/bin/
+
+rsync -avx --progress "./dist/usd-tk-tt-q3.arm64.$dt" way:/usr/local/bin/
+ssh way "rsync -avx --progress /usr/local/bin/usd-tk-tt-q3.arm64.$dt nv1:/usr/local/bin/"
+ssh way "rsync -avx --progress /usr/local/bin/usd-tk-tt-q3.arm64.$dt nv2:/usr/local/bin/"
--- applications/usd-tk-tt-q3/init.go
+++ applications/usd-tk-tt-q3/init.go
@@ -3,5 +3,5 @@ package main
 import "github.com/geometrybase/hft-micro/logger"
 
 func init() {
-	logger.Debug("####  BUILD @ 20211129 17:35:50  ####")
+	logger.Debug("####  BUILD @ 20211129 17:50:23  ####")
 }
