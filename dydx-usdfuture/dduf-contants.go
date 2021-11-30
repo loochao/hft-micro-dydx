a77f29a6eb4c243c954d663b1f85a616e2afcf4e
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-11-30 21:21:29 +0800

    build usd-tk-tt-q3.20211130

--- dydx-usdfuture/dduf-contants.go
+++ dydx-usdfuture/dduf-contants.go
@@ -28,10 +28,10 @@ const (
 	ExchangeID = common.DydxUsdFuture
 
 	depthReadPoolSize = 8192
-	depthReadMsgSize  = 4096
+	depthReadMsgSize  = 2048
 
-	userReadPoolSize = 4096
-	userReadMsgSize  = 4096
+	userReadPoolSize = 1024
+	userReadMsgSize  = 8192*2
 )
 
 const (
