621d1efec6382b3dc4b059c1700eea023a7e1c0e
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-12-08 22:59:37 +0800

    build usd-tk-tt-q3.20211208

--- applications/usd-tk-tt-q3/build.sh
+++ applications/usd-tk-tt-q3/build.sh
@@ -20,7 +20,7 @@ env GOOS=linux GOARCH=amd64 go build -o "./dist/usd-tk-tt-q3.amd64.$dt" ./applic
 chmod 755 "./dist/usd-tk-tt-q3.amd64.$dt"
 chmod 755 "./dist/usd-tk-tt-q3.arm64.$dt"
 
-rsync -avx --progress "./dist/usd-tk-tt-q3.amd64.$dt" hkhr:/usr/local/bin/
+rsync -avx --progress "./dist/usd-tk-tt-q3.arm64.$dt" tka3:/usr/local/bin/
 
 rsync -avx --progress "./dist/usd-tk-tt-q3.arm64.$dt" arm1:/usr/local/bin/
 rsync -avx --progress "./dist/usd-tk-tt-q3.arm64.$dt" arm4:/usr/local/bin/
