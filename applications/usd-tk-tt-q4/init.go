abdea827fa1fe6a19a6d11114b20daca6579d9d4
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2021-12-18 18:06:37 +0800

    build usd-tk-tt-q4.20211218

--- applications/usd-tk-tt-q4/init.go
+++ applications/usd-tk-tt-q4/init.go
@@ -3,5 +3,5 @@ package main
 import "github.com/geometrybase/hft-micro/logger"
 
 func init() {
-	logger.Debug("####  BUILD @ 20211218 02:52:16  ####")
+	logger.Debug("####  BUILD @ 20211218 10:06:37  ####")
 }
--- dydx-usdfuture/dduf-api.go
+++ dydx-usdfuture/dduf-api.go
@@ -9,6 +9,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/geometrybase/hft-micro/common"
+	"github.com/geometrybase/hft-micro/logger"
 	"io/ioutil"
 	"net"
 	"net/http"
@@ -109,7 +110,7 @@ func (api *API) SendAuthenticatedHTTPRequest(ctx context.Context, method, path s
 	//if method == http.MethodPost {
 	//	logger.Debugf("%s %s", method, path)
 	//} else {
-		//logger.Debugf("%s", contents)
+		logger.Debugf("%s", contents)
 	//}
 	if err != nil {
 		return err
